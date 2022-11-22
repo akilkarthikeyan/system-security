80e41349213271f363cf544df48d6d68d6f6cd47
Akil Karthikeyan <76770419+akilkarthikeyan@users.noreply.github.com>
2022-11-22 08:58:19 +0530

    Create A3.py

---
+++ codes/A3.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import requests
+import uvicorn
+
+app = FastAPI()
+ports = [7001, 8002]
+
+@app.get('/')
+def index():
+    return "A3"
+
+@app.get('/getConstructorInfo')
+def constructorInfo():
+    data = {}
+    final = {}
+    for port in ports:
+        URL = "http://127.0.0.1:" + str(port) + "/getConstructorInfo"
+        r = requests.get(url = URL)
+        temp = r.json()
+        for key in temp:
+            data[key] = temp[key]
+    final["A3"] = data
+    return final
+
+if __name__ == '__main__':
+    uvicorn.run(app, port = 8003, host = '127.0.0.1')
